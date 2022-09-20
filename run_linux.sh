7ddbfb5ac0c629975c7f5d95f78af2b5e8827113
Arun.KP <arunkp1986@gmail.com>
2022-09-20 17:26:32 +0530

    misc: run script
    Change-Id: I8b66453c883411824e0d9540779387ad317516b6

---
+++ run_linux.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+outdir=$1
+bench=$2
+cwd=$(pwd)
+
+if [[ -f "PhyMem.0" ]];then
+rm PhyMem.0
+fi
+if [[ -f "PhyMem.1" ]];then
+rm PhyMem.1
+fi
+
+./build/X86/gem5.opt --outdir=$outdir configs/spec_config/run_spec.py $cwd"/fullsystem_images/vmlinux-v5.2.3" $cwd"/fullsystem_images/spec-2017-image/spec-2017" "timing" "$bench" "test"
+
