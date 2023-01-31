9d55cdfde31f706e7d5f305cb6dfa1613c506110
arunkp <arunkp1986@gmail.com>
2023-01-31 10:07:34 +0000

    misc: scripts
    Change-Id: Ibfc4204c575677fd7e598990aacb384d84707081

--- run_nobypass.sh
+++ run_nobypass.sh
@@ -1,14 +1,21 @@
 #!/bin/bash
 
 outdir=$1
+kernel=$2
+disk=$3
 
-if [[ $# -ne 1 ]];then
-    echo "pass output dir"
+if [[ $# -ne 3 ]];then
+    echo "pass output dir,kernel,disk"
     exit 2
 fi
 
 rm PhyMem.0
 rm PhyMem.1
 
-./build/X86/gem5.opt --outdir=$outdir configs/example/fs.py --mem-size=3GB --nvm-size=2GB --caches --l3cache --cpu-type TimingSimpleCPU --hybrid-channel True --mem-type=DDR4_2400_16x4 --nvm-type=NVM_2666_1x64 --kernel /home/cse/arunkp/stack_persistence/gemOS_nvm_ssp/gemOS.kernel --disk-image /home/cse/arunkp/stack_persistence/gem5/fullsystem_images/data_gapbs.img
+echo $outdir
+echo $kernel
+echo $disk
+
+
+./build/X86/gem5.opt --outdir=$outdir configs/example/fs.py --mem-size=3GB --nvm-size=2GB --caches --l3cache --cpu-type TimingSimpleCPU --hybrid-channel True --mem-type=DDR4_2400_16x4 --nvm-type=NVM_2666_1x64 --kernel=$kernel --disk-image=$disk
 
