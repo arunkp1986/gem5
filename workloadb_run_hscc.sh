7879e1f0819c5fb69c7b41b444573b3f4aef3b39
arunkp <arunkp1986@gmail.com>
2023-09-08 07:56:17 +0000

    misc:remove
    Change-Id: I28ce9be4524062493ff32ea478a56661c6f41507

--- workloadb_run_hscc.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./run_nobypass.sh /home/kparun/hybrid_work/output/gemOS/hscc/nomigration/workloadb /home/kparun/hybrid_work/gemOS_nvm_hscc/gemOS_hscc.kernel /home/kparun/hybrid_work/motivation/simpoint/data_workloadb.img > workloadb_hscc.out 2>&1
